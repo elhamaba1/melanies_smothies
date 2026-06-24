986b189403a342609d32b35794d486aeb07035fd
elhamaba1 <elham.babaee@at.govt.nz>
2026-06-24 18:26:54 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,14 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
+
 if ingredients_list:
     ingredients_string =''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string+= fruit_chosen + ' '
+      ingredients_string+= fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -44,9 +47,6 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get(
-   "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
   
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
+#sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
