edcef69c3a6c737bad21925bcaa27d01705a239d
elhamaba1 <elham.babaee@at.govt.nz>
2026-06-24 14:03:23 +1200

    Fix formatting and spacing in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The Name of your Smoothie will be: " , name_on_order)
 
-cnx=st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
-#session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
